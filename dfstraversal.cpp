d1684f7001f7c33c9f6c5c999980b0bcc2f01199
Khushaal Sharma <97724393+khushaalsharma@users.noreply.github.com>
2022-12-27 17:09:23 +0530

    Create dfstraversal.cpp

---
+++ dfstraversal.cpp
@@ -0,0 +1,32 @@
+#include <unordered_map>
+#include <set>
+#include <vector>
+void dfs(unordered_map<int,set<int>> &adj, unordered_map<int,bool>&visit, int node, vector<int>&ans){
+    visit[node] = true;
+    ans.push_back(node);
+    for(auto i:adj[node]){
+        if(!visit[i]){
+            dfs(adj,visit,i,ans);
+        }
+    }
+}
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+    unordered_map<int,set<int>> adj;
+    unordered_map<int,bool> visit;
+    vector<vector<int>> result;
+    for(int i=0;i<edges.size();i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].insert(v);
+        adj[v].insert(u);
+    }
+    for(int i=0;i<V;i++){
+        if(!visit[i]){
+            vector<int> ans;
+            dfs(adj,visit,i,ans);
+            result.push_back(ans);
+        }
+    }
+    return result;
+}
